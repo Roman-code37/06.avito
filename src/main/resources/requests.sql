b6143d548cc8a41357043aeb2d713cdb814e9150
Roman <groma_92@mail.ru>
2019-11-23 10:52:52 +0300

    initial comit

--- src/main/resources/requests.sql
+++ src/main/resources/requests.sql
@@ -11,4 +11,3 @@ insert into houses values (3,3150000, 'Советский', 'Горки');
 insert into houses values (4,3000000, 'Приволжсий', 'Проспект победы');
 insert into houses values (5,2750000, 'Приволжсий', 'Горки');
 insert into houses values (6,3280000, 'Авиастроительный', 'Авиастроительная');
-
