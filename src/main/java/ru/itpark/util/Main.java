cdbaf7780bc6a07a4c51bc9ec0a0825cf58f4559
Roman <groma_92@mail.ru>
2019-11-23 10:31:52 +0300

    initial comit

---
+++ src/main/java/ru/itpark/util/Main.java
@@ -0,0 +1,17 @@
+package ru.itpark.util;
+
+import com.sun.tracing.dtrace.NameAttributes;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/resources/requests.sql
@@ -0,0 +1,14 @@
+create table houses (
+    id integer primary key autoincrement,
+    price integer check ( price > 0 ),
+    district text,
+    underground text
+);
+
+insert into houses values (1,3000000, 'Приволжсий', 'Горки');
+insert into houses values (2,2800000, 'Вахитовский', 'Суконная слобода');
+insert into houses values (3,3150000, 'Советский', 'Горки');
+insert into houses values (4,3000000, 'Приволжсий', 'Проспект победы');
+insert into houses values (5,2750000, 'Приволжсий', 'Горки');
+insert into houses values (6,3280000, 'Авиастроительный', 'Авиастроительная');
+
